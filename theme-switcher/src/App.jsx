4daaa2cb87bff1f3bcadaf433647ce6e83c0aa04
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-07-22 19:34:44 +0530

    add Theme switcher context api setup

---
+++ theme-switcher/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-pink-500 p-4 text-white text-3xl'>React and Context Api industry standard</h1>
+    </>
+  )
+}
+
+export default App
