240a1e981a5991d64711d270ff6a7c74939e23ea
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-07-22 20:26:45 +0530

    add better syntax for Context Api and complete the functionality

--- theme-switcher/src/App.jsx
+++ theme-switcher/src/App.jsx
@@ -1,13 +1,44 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
+	const [themeMode, setThemeMode] = useState('light')
 
-  return (
-    <>
-      <h1 className='bg-pink-500 p-4 text-white text-3xl'>React and Context Api industry standard</h1>
-    </>
-  )
+	const lightMode = () => {
+		setThemeMode('light')
+	}
+
+	const darkMode = () => {
+		setThemeMode('dark')
+	}
+
+	// Actual change in theme
+	useEffect(() => {
+		const html = document.querySelector('html')
+		html.classList.remove('light', 'dark')
+		html.classList.add(themeMode)
+	})
+
+	return (
+		<ThemeProvider value={{themeMode, lightMode, darkMode}}>
+			<div className="flex flex-wrap min-h-screen items-center">
+				<div className="w-full">
+					<div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+						<ThemeBtn />
+					</div>
+
+					<div className="w-full max-w-sm mx-auto">
+						<Card />
+					</div>
+				</div>
+			</div>
+		</ThemeProvider>
+
+	)
 }
 
 export default App
