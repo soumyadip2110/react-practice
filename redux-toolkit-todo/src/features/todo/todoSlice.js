feb2276a827f6ad9e247cd6b6bb4ce17dfefabf5
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-07-24 22:29:26 +0530

    configure store and slice for redux-toolkit

---
+++ redux-toolkit-todo/src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{id: 1, text: 'hello world'}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState: initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload.text
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload.id)
+        },
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
